97fe15459b6186e7112febed8b3612368bab61cd
Manuel Montero <87241159+ManuelMC21@users.noreply.github.com>
2025-09-01 12:50:02 -0400

    DeployNuGet

--- obj/Debug/net9.0/OnliZassData.AssemblyInfo.cs
+++ obj/Debug/net9.0/OnliZassData.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnliZassData")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44d731404ba907ae6d04dc5d2dc3b1a974defe34")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnliZassData")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnliZassData")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
